35e7c9a27dc62762254e3e1b53f5567a6f344592
Shovon Halder <shovonhalder00@gmail.com>
2024-09-28 22:00:30 +0530

    Your commit message1

--- .history/agri/src/signupform_20240918214742.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function signupform() {
-  return (
-    <div className='sigh'>signupform</div>
-  )
-}
--- .history/agri/src/signupform_20240918214837.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function signupform() {
-  return (
-      <div className='signup-container'>
-          <f
-    </div>
-  )
-}
